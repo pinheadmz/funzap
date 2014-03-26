2f6a286e838c0150a9203d590d8ee7df8381260f
pinheadmz <pinheadmz@p3nlhftpg125.shr.prod.phx3.secureserver.net>
2014-03-26 16:50:58 -0700

    really init now

--- safe/header.php
+++
@@ -1,6 +0,0 @@
-<?
-?>
-<div id="header">
-	<div id="headertitle">Fun Zap!</div>
-	<div id="headersubtitle">Make and share personalized social games with your friends</div>
-</div>
--- safe/urlcommon.php
+++
@@ -1,8 +0,0 @@
-<?
-	// this doc only visible when embedded in homepage like its supposed to -- no cheating!
-	if(($_SERVER["REQUEST_URI"] != "/funzap/") && ($_SERVER["REQUEST_URI"] != "/funzap/index.php"))
-	{
-		header( 'Location: http://www.MatthewZipkin.com/funzap' ) ;
-		exit();
-	}
-?>
